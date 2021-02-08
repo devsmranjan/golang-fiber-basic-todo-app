0268ab36d63899cdd4dfdd0a2502c8cc9e2d83eb
smrutiranjanrana <smrutiranjan.developer@gmail.com>
2021-02-08 18:16:49 +0530

    [UPDATE] controller

--- controllers/todo.go
+++ controllers/todo.go
@@ -7,26 +7,27 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
+// Todo : todo model
 type Todo struct {
-	Id        int    `json:"id"`
-	Title      string `json:"title"`
+	ID        int    `json:"id"`
+	Title     string `json:"title"`
 	Completed bool   `json:"completed"`
 }
 
 var todos = []*Todo{
 	{
-		Id:        1,
-		Title:      "Walk the dog 🦮",
+		ID:        1,
+		Title:     "Walk the dog 🦮",
 		Completed: false,
 	},
 	{
-		Id:        2,
-		Title:      "Walk the cat 🐈",
+		ID:        2,
+		Title:     "Walk the cat 🐈",
 		Completed: false,
 	},
 }
 
-// get all todos
+// GetTodos : get all todos
 func GetTodos(c *fiber.Ctx) error {
 	return c.Status(fiber.StatusOK).JSON(fiber.Map{
 		"success": true,
@@ -36,7 +37,7 @@ func GetTodos(c *fiber.Ctx) error {
 	})
 }
 
-// Create a todo
+// CreateTodo : Create a todo
 func CreateTodo(c *fiber.Ctx) error {
 	type Request struct {
 		Title string `json:"title"`
@@ -50,15 +51,15 @@ func CreateTodo(c *fiber.Ctx) error {
 	if err != nil {
 		fmt.Println(err)
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
-			"success":  false,
+			"success": false,
 			"message": "Cannot parse JSON",
 		})
 	}
 
 	// create a todo variable
 	todo := &Todo{
-		Id:        len(todos) + 1,
-		Title:      body.Title,
+		ID:        len(todos) + 1,
+		Title:     body.Title,
 		Completed: false,
 	}
 
@@ -73,26 +74,26 @@ func CreateTodo(c *fiber.Ctx) error {
 	})
 }
 
-// get a single todo
+// GetTodo : get a single todo
 // PARAM: id
 func GetTodo(c *fiber.Ctx) error {
 	// get parameter value
-	paramId := c.Params("id")
+	paramID := c.Params("id")
 
 	// convert parameter value string to int
-	id, err := strconv.Atoi(paramId)
+	id, err := strconv.Atoi(paramID)
 
 	// if error in parsing string to int
 	if err != nil {
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
-			"success":  false,
+			"success": false,
 			"message": "Cannot parse Id",
 		})
 	}
 
 	// find todo and return
 	for _, todo := range todos {
-		if todo.Id == id {
+		if todo.ID == id {
 			return c.Status(fiber.StatusOK).JSON(fiber.Map{
 				"success": true,
 				"data": map[string]*Todo{
@@ -109,14 +110,14 @@ func GetTodo(c *fiber.Ctx) error {
 	})
 }
 
-// Update a todo
+// UpdateTodo : Update a todo
 // PARAM: id
 func UpdateTodo(c *fiber.Ctx) error {
 	// find parameter
-	paramId := c.Params("id")
+	paramID := c.Params("id")
 
 	// convert parameter string to int
-	id, err := strconv.Atoi(paramId)
+	id, err := strconv.Atoi(paramID)
 
 	// if parameter cannot parse
 	if err != nil {
@@ -128,7 +129,7 @@ func UpdateTodo(c *fiber.Ctx) error {
 
 	// request structure
 	type Request struct {
-		Title      *string `json:"title"`
+		Title     *string `json:"title"`
 		Completed *bool   `json:"completed"`
 	}
 
@@ -145,15 +146,15 @@ func UpdateTodo(c *fiber.Ctx) error {
 	var todo *Todo
 
 	for _, t := range todos {
-		if t.Id == id {
+		if t.ID == id {
 			todo = t
 			break
 		}
 	}
 
-	if todo.Id == 0 {
+	if todo.ID == 0 {
 		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
-			"success":  false,
+			"success": false,
 			"message": "Not found",
 		})
 	}
@@ -174,14 +175,14 @@ func UpdateTodo(c *fiber.Ctx) error {
 	})
 }
 
-// Delete a todo
+// DeleteTodo : Delete a todo
 // PARAM: id
 func DeleteTodo(c *fiber.Ctx) error {
 	// get param
-	paramId := c.Params("id")
+	paramID := c.Params("id")
 
 	// convert param string to int
-	id, err := strconv.Atoi(paramId)
+	id, err := strconv.Atoi(paramID)
 
 	// if parameter cannot parse
 	if err != nil {
@@ -193,13 +194,13 @@ func DeleteTodo(c *fiber.Ctx) error {
 
 	// find and delete todo
 	for i, todo := range todos {
-		if todo.Id == id {
+		if todo.ID == id {
 
 			todos = append(todos[:i], todos[i+1:]...)
 
 			return c.Status(fiber.StatusNoContent).JSON(fiber.Map{
-				"success":  true,
-				"message": "Deleted Succesfully",
+				"success": true,
+				"message": "Deleted Successfully",
 			})
 		}
 	}
